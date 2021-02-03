391770f91a6797a57feabf92e4a0027cc584ecd7
nicolassalless <75262708+nicolassalless@users.noreply.github.com>
2021-02-03 08:50:18 -0600

    Update collectable.js

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -36,8 +36,42 @@
         ////////////////////////////////////////////////////////////////////////
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
         
-        // example: 
-        createCollectable(type.steve, 200, 170, 6, 0.7);
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    let opspark = window.opspark;
+    window.opspark.collectable = window.opspark.collectable || {};
+    
+    opspark.collectable.factory = function (game) {
+        game.collectable = game.add.group();
+        game.collectable.enableBody = true;
+        
+        function create(type, x, y, gravity, bounce) {
+            var collectable = game.collectable.create(x, y, type.assetKey);
+            collectable.type = type;
+            
+            /*
+             * Extra adjustments or configuration.
+             */
+            switch (type.assetKey) {
+                case 'db':
+                    collectable.body.height = 44;
+                    break;
+                    
+                case 'grace':
+                case 'kennedi':
+                    collectable.scale.x = collectable.scale.y = 0.9;
+                    break;
+                
+                default:
+                    // code
+            }
+            if (gravity) collectable.body.gravity.y = gravity;
+            if (bounce) collectable.body.bounce.y = bounce + Math.random() * 0.2;
+        }
+        opspark.collectable.create = create;
+    };
+})(window);
         
         
         
