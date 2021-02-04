ca0d844baaceff40628931eaf175dd85347b5359
nicolassalless <nicolassalles829@gmail.com>
2021-02-04 14:48:36 +0000

    reinstall projects

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -36,37 +36,8 @@
         ////////////////////////////////////////////////////////////////////////
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
         
-
-    opspark.collectable.factory = function (game) {
-        game.collectable = game.add.group();
-        game.collectable.enableBody = true;
-        
-        function create(type, x, y, gravity, bounce) {
-            var collectable = game.collectable.create(x, y, type.assetKey);
-            collectable.type = type;
-            
-            /*
-             * Extra adjustments or configuration.
-             */
-            switch (type.assetKey) {
-                case 'db':
-                    collectable.body.height = 44;
-                    break;
-                    
-                case 'max':
-                case 'steve':
-                    collectable.scale.x = collectable.scale.y = 0.9;
-                    break;
-                
-                default:
-                    // code
-            }
-            if (gravity) collectable.body.gravity.y = gravity;
-            if (bounce) collectable.body.bounce.y = bounce + Math.random() * 0.2;
-        }
-        opspark.collectable.create = create;
-    };
-})(window);
+        // example: 
+        createCollectable(type.steve, 200, 170, 6, 0.7);
         
         
         
