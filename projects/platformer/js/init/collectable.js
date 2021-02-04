4e19de32dab64f3b784285a7eb6c0b6276b1e968
nicolassalless <75262708+nicolassalless@users.noreply.github.com>
2021-02-04 08:32:14 -0600

    Update collectable.js

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -58,8 +58,8 @@
                     collectable.body.height = 44;
                     break;
                     
-                case 'grace':
-                case 'kennedi':
+                case '':
+                case '':
                     collectable.scale.x = collectable.scale.y = 0.9;
                     break;
                 
